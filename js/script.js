8eca2f3bdaba352097fb1e8332aff55061fab5dd
Hiển Nguyễn Vũ <zino@Zino-Mac.local>
2022-10-24 21:46:50 +0700

    init js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const songs = [
+    {
+        name: 'Waiting For You',
+        singer: 'MONO',
+        path: './audio/WaitingForYou.mp3',
+        thumbnail: './img/waiting-for-you.jpg'
+    },
+    {
+        name: 'Hương',
+        singer: 'Văn Mai Hương',
+        path: './audio/Huong.mp3',
+        thumbnail: './img/huong.jpg'
+    },
+    {
+        name: 'Hãy Trao Cho Anh',
+        singer: 'Sơn Tùng M-TP',
+        path: './audio/HayTraoChoAnh.mp3',
+        thumbnail: './img/hay-trao-cho-anh.jpg'
+    },
+    {
+        name: 'Tòng Phu',
+        singer: 'KEYO',
+        path: './audio/TongPhu.mp3',
+        thumbnail: './img/tong-phu.jpg'
+    },
+]
